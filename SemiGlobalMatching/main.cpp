5f0f95e309263bed029541d749c74be6a213ddab
jerryweihuajing <jerryweihuajing@126.com>
2022-04-22 17:51:04 +0800

    batch process image pair

--- SemiGlobalMatching/main.cpp
+++ SemiGlobalMatching/main.cpp
@@ -26,6 +26,91 @@ void SaveDisparityMap(const float32* disp_map, const sint32& width, const sint32
 /*保存视差点云*/
 void SaveDisparityCloud(const uint8* img_bytes, const float32* disp_map, const sint32& width, const sint32& height, const std::string& path);
 
+#define LEFTSTRIP 0
+#define RIGHTSTRIP 1
+#define BOTHSTRIP 2
+
+std::string do_strip(const std::string& str, int striptype, const std::string& chars)
+{
+    std::string::size_type strlen = str.size();
+    std::string::size_type charslen = chars.size();
+    std::string::size_type i, j;
+
+    //默认情况下，去除空白符
+    if (0 == charslen)
+    {
+        i = 0;
+        //去掉左边空白字符
+        if (striptype != RIGHTSTRIP)
+        {
+            while (i < strlen && ::isspace(str[i]))
+            {
+                i++;
+            }
+        }
+        j = strlen;
+        //去掉右边空白字符
+        if (striptype != LEFTSTRIP)
+        {
+            j--;
+            while (j >= i && ::isspace(str[j]))
+            {
+                j--;
+            }
+            j++;
+        }
+    }
+    else
+    {
+        //把删除序列转为c字符串
+        const char* sep = chars.c_str();
+        i = 0;
+        if (striptype != RIGHTSTRIP)
+        {
+            //memchr函数：从sep指向的内存区域的前charslen个字节查找str[i]
+            while (i < strlen && memchr(sep, str[i], charslen))
+            {
+                i++;
+            }
+        }
+        j = strlen;
+        if (striptype != LEFTSTRIP)
+        {
+            j--;
+            while (j >= i && memchr(sep, str[j], charslen))
+            {
+                j--;
+            }
+            j++;
+        }
+        //如果无需要删除的字符
+        if (0 == i && j == strlen)
+        {
+            return str;
+        }
+        else
+        {
+            return str.substr(i, j - i);
+        }
+    }
+
+}
+
+std::string strip(const std::string& str, const std::string& chars = " ")
+{
+    return do_strip(str, BOTHSTRIP, chars);
+}
+
+std::string lstrip(const std::string& str, const std::string& chars = " ")
+{
+    return do_strip(str, LEFTSTRIP, chars);
+}
+
+std::string rstrip(const std::string& str, const std::string& chars = " ")
+{
+    return do_strip(str, RIGHTSTRIP, chars);
+}
+
 //------------------------------------------------------------------------------
 /*
 Calculate the path of all the files under the path
@@ -362,7 +447,10 @@ int main(int argv, char** argc)
         //在string中寻找sub_string
         if (item.find("left.bmp") != string::npos) {
         
-            std::cout << split(item, "/").back() << std::endl;
+            string this_image_name = strip(split(item, "/").back(), "_left.bmp");
+
+            std::cout << this_image_name << std::endl;
+            SGMFromImageName(folder_path, this_image_name);
         }
     }
 
